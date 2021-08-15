f2995303fbf54fc58ef3d1568c3d09888d6e8abd
Simon Brisebois-Therrien <simonbt6@gmail.com>
2021-08-15 13:04:35 -0400

    Handshake sequence struct

--- src/core/RTMPHandshake.hpp
+++ src/core/RTMPHandshake.hpp
@@ -11,6 +11,21 @@
  **/
 namespace Handshake
 {
+    enum State
+    {
+        /** 
+         * The protocol version is sent during this stage. 
+         * Both the client and server are uninitialized.
+         * The client sends the protocol version in C0.
+         * If the server supports the version, it sends S0
+         * and S1 in response.
+         **/
+        Uninitialized = 0,
+        VersionSent = 1,
+        AcknowledgeSent = 2,
+        DoneSent = 3
+    };
+    
     // C0 & S0
     struct F0 
     {
@@ -21,7 +36,7 @@ namespace Handshake
     struct F1 
     {
         unsigned int time[TIME_BYTES_COUNT];
-        unsigned char randomBytes[RANDOM_BYTES_COUNT];
+        unsigned int randomBytes[RANDOM_BYTES_COUNT];
     };
 
     // C2 & S2
@@ -29,6 +44,25 @@ namespace Handshake
     {
         unsigned int time[TIME_BYTES_COUNT];
         unsigned int time2[TIME_BYTES_COUNT];
-        unsigned char randomBytes[RANDOM_BYTES_COUNT];
+        unsigned int randomBytes[RANDOM_BYTES_COUNT];
+    };
+
+    struct Handshake
+    {
+        // Status
+        State state = State::Uninitialized;
+
+        // 
+        F0 C0;
+        F0& S0 = C0;
+
+        //
+        F1 C1;
+        F1 S1;
+
+        //
+        F2 C2;
+        F2 S2;
+        
     };
 }
