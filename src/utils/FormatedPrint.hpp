b57df8aba0adf2b77be5502830f39dca6922c55a
Simon Brisebois-Therrien <simonbt6@gmail.com>
2021-08-21 13:09:18 -0400

    Formated Print for bytes.

---
+++ src/utils/FormatedPrint.hpp
@@ -0,0 +1,35 @@
+/**
+ * Author: Simon Brisebois-Therrien 
+ * Date: 2021-08-21
+ **/
+#pragma once
+
+#include <iostream>
+
+namespace Utils 
+{
+    class FormatedPrint
+    {
+        public:
+        /**
+         * Ex: 
+         * FF FF FF FF FF FF FF FF   FF FF FF FF FF FF FF FF
+         * FF FF FF FF FF FF FF FF   FF FF FF FF FF FF FF FF
+         * FF FF FF FF FF FF FF FF   FF FF FF FF FF FF FF FF
+         * ...
+         **/
+        static void PrintBytes(int* arr, int length)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                if (i % 16 == 0) printf("\n");
+                else if (i % 8 == 0) printf("   ");
+
+                printf("%i  ", arr[i]);
+
+                if (arr[i] < 10) printf("  ");
+                else if (arr[i] < 100) printf(" ");
+            }
+        }
+    };
+}
