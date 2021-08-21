c47e2262cfba1d8ee2e8d1ad55f2d9bec1cfd660
Simon Brisebois-Therrien <simonbt6@gmail.com>
2021-08-21 12:55:10 -0400

    Print byte formating

--- src/core/RTMPParser.cpp
+++ src/core/RTMPParser.cpp
@@ -15,6 +15,9 @@ namespace RTMP {
     {
         for (int i = 0; i < length; i++)
         {
+            if (i % 16 == 0) printf("\n");
+            else if (i % 8 == 0) printf("   ");
+            
             printf("\n%i", arr[i]);
         }
     }
