77da9953b107f68c6ba72ec7037a98f3978262b1
Simon Brisebois-Therrien <simonbt6@gmail.com>
2021-08-29 14:26:25 -0400

    Fix mantissa

--- src/utils/Math.hpp
+++ src/utils/Math.hpp
@@ -15,17 +15,27 @@ namespace Utils
     {
         public:
 
+            static double CalculateMantissa(vector<unsigned char>& bits, int low, int high)
+            {
+                double mantissa = 0.00;
+                for (int i = 0; i < (high - low + 1); i++)
+                    mantissa += ((double)(bits.at(i + low)))*pow(2, -(i + 1));
+                return mantissa;
+            }
+
+
             static double IE754ToDouble(unsigned char* bytes)
             {
                 vector<unsigned char> bits = BitOperations::BytesToBits(bytes);
+                double mantissa;
                 unsigned int
-                    mantissa, 
                     exponent, 
-                    sign = (bits[0])? -1 : 1, 
+                    sign = bits[0], 
                     bias = 1023;                
-                mantissa = BitOperations::BitsToInteger(bits, 12, 63);
+                mantissa = CalculateMantissa(bits, 12, 63);
                 exponent = BitOperations::BitsToInteger(bits, 1, 11);
-                double dValue = sign * std::pow(2, (exponent - bias)) * (1 + mantissa);
+
+                double dValue = std::pow(-1, sign) * std::pow(2, (exponent - bias)) * (1.0 + mantissa);
                 return 
                     (dValue == std::numeric_limits<double>::infinity())? 1 : dValue;
             }
